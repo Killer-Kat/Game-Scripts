da4d337419cb65985445acf8f2213978df0f9c16
Killer-Kat <goldkat99@gmail.com>
2021-02-24 15:36:42 -0500

    Framework for the leveling system.

--- PlayerStats.cs
+++ PlayerStats.cs
@@ -6,17 +6,39 @@ using UnityEngine.UI;
 public class PlayerStats : MonoBehaviour
 {
     public int playerLevel = 1;
+    public int maxLevel = 100;
     public Text levelText;
     public int currentExp;
+    public int baseExp = 1000;
+    public int[] expToLevelup;
     // Start is called before the first frame update
     void Start()
     {
         levelText.text = "Level: " + playerLevel;
+        expToLevelup = new int[maxLevel];
+        expToLevelup[1] = baseExp;
+        for(int i = 2; i < expToLevelup.Length; i++)
+        {
+            expToLevelup[i] = Mathf.FloorToInt(expToLevelup[i - 1] * 1.05f);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public void giveExp(int expToGive)
+    {
+        currentExp = currentExp + expToGive;
+        upDateStats();
+    }
+    void upDateStats()
+    {
+        levelText.text = "Level: " + playerLevel;
+        if (expToLevelup[playerLevel] + expToLevelup[playerLevel + 1] >= currentExp)
+        {
+            playerLevel++;
+        }
     }
 }
