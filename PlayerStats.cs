4deb4814582808679062984f6ba547a39f2a95b9
Killer-Kat <goldkat99@gmail.com>
2021-02-24 15:04:55 -0500

    Create PlayerStats.cs

---
+++ PlayerStats.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerStats : MonoBehaviour
+{
+    public int playerLevel = 1;
+    public Text levelText;
+    public int currentExp;
+    // Start is called before the first frame update
+    void Start()
+    {
+        levelText.text = "Level: " + playerLevel;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
